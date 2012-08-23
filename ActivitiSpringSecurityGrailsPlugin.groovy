2d7ff062babfa51122ea53a126e4bb24cbc754e3
Lim Chee Kin <limcheekin@vobject.com>
2012-08-23 16:23:17 +0800

    0.4.9 release for loadAfter fix

--- ActivitiSpringSecurityGrailsPlugin.groovy
+++ ActivitiSpringSecurityGrailsPlugin.groovy
@@ -23,7 +23,7 @@ import org.grails.activiti.ActivitiConstants
  */
 class ActivitiSpringSecurityGrailsPlugin {
 	// the plugin version
-	def version = "0.4.8"
+	def version = "0.4.9"
 	// the version or versions of Grails the plugin is designed for
 	def grailsVersion = "1.3.3 > *"
 	// resources that are excluded from plugin packaging
